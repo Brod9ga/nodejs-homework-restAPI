765ea5a0d4cd69d786ebf5671c06e9bae452ebbd
Brod9ga <119956367+Brod9ga@users.noreply.github.com>
2023-09-27 23:43:51 +0300

    1

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -6,15 +6,21 @@ import authenticate from "../../middlewares/authenticate.js";
 import upload from "../../middlewares/upload.js";
 import uploadAvatar from "../../middlewares/uploadAvatar.js";
 
+
 const authRouter = express.Router();
 
 const userSignupSchema = validateBody(userSchemas.userSignupSchema)
 const userSigninSchema = validateBody(userSchemas.userSigninSchema);
 const userUpdateAvatarSchema = validateBody(userSchemas.userUpdateAvatar)
+const userEmailSchema = validateBody(userSchemas.userEmailSchema);
 
 authRouter.post("/register", userSignupSchema, authCTRL.signup);
 
 authRouter.post("/login", userSigninSchema, authCTRL.signin);
+
+authRouter.get("/verify/:verificationToken", authCTRL.verify);
+
+authRouter.post("/verify", userEmailSchema, authCTRL.resendVerifyEmail);
 
 authRouter.get("/current", authenticate, authCTRL.getCurrent)
 
